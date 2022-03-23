9f3a6880624f0a57bbc9f747b7a3b47576cd8b77
PROVILLE Maximilien <maximilien.proville77@gmail.com>
2022-03-23 17:13:40 +0100

    23-03 17:13 --> Modal Touch

--- dist/components/PopModal 2.js
+++ dist/components/PopModal 2.js
@@ -13,9 +13,9 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 const PopUp = props => {
   return props.trigger ? /*#__PURE__*/_react.default.createElement("div", {
-    className: "fixed top-0 left-0 w-full h-screen flex justify-center items-center"
+    className: "fixed top-0 left-0 w-full h-screen flex justify-center items-center ".concat(props.bgView ? "bg-purple-900/50" : "")
   }, /*#__PURE__*/_react.default.createElement("div", {
-    className: "relative p-6 w-full max-w-xl bg-white rounded-xl border-4 border-purple-500 shadow-lg shadow-purple-500"
+    className: "relative p-6 w-full max-w-xl bg-white rounded-xl border-4 border-purple-500 shadow-lg shadow-purple-500 ".concat(props.animationOn ? "animate-bounce" : "")
   }, /*#__PURE__*/_react.default.createElement("button", {
     className: "absolute right-4 top-4 text-3xl text-purple-500 font-extrabold ",
     onClick: () => props.setTrigger(false)
--- src/lib/components/PopModal.js
+++ src/lib/components/PopModal.js
@@ -2,17 +2,12 @@ import React from "react";
 import { FiX } from "react-icons/fi";
 
 export const PopUp = (props) => {
-  console.log(props.bgView);
   return props.trigger ? (
     <div
-      className={`fixed top-0 left-0 w-full h-screen flex justify-center items-center ${
-        props.bgView ? `bg-purple-900/50` : ``
-      }`}
+      className={`fixed top-0 left-0 w-full h-screen flex justify-center items-center`}
     >
       <div
-        className={`relative p-6 w-full max-w-xl bg-white rounded-xl border-4 border-purple-500 shadow-lg shadow-purple-500 ${
-          props.animationOn ? `animate-bounce` : ``
-        }`}
+        className={`relative p-6 w-full max-w-xl bg-white rounded-xl border-4 border-purple-500 shadow-lg shadow-purple-500`}
       >
         <button
           className="absolute right-4 top-4 text-3xl text-purple-500 font-extrabold "
